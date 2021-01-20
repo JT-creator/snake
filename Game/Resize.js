a7f67ab971fddbdc1abe859c89ab95b53c157e5c
unknown <ejtzhang@163.com>
2021-01-20 17:28:19 +0800

    'add

--- Game/Resize.js
+++ Game/Resize.js
@@ -3,14 +3,15 @@ resize();
 function resize()
 {
     const Hwid = document.body.clientWidth;
-    const Hhei =document.body.clientHeight;
+    const Hhei = document.body.clientHeight;
 
-    let b = Math.min( (0.5 * Hhei) / bRows, (0.75 * Hhei) / bCols );
+    let b = Math.min( (0.75 * Hwid) / bRows, (0.75 * Hhei) / bCols );
+    console.log((0.75 * Hwid) / bRows, (0.75 * Hhei) / bCols );
     b = Math.floor(b);
 
     var can = document.getElementById("Canvas_main");
     can.setAttribute("width", b*bCols );
-    can.setAttribute("height", b*bRows * 1.5 );
+    can.setAttribute("height", b*bRows + 75 );
 }
 
 
